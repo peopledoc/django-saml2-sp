4f821367dde0f29d065404a6fc88d39ba067499f
guitarman@vt.edu <guitarman@vt.edu@120867ed-ad96-fafc-8a43-e235d9f8a922>
2010-08-20 17:00:13 +0000

    Log in the user locally when an assertion is returned from the IDP.
    git-svn-id: http://django-saml2-sp.googlecode.com/svn/trunk@8 120867ed-ad96-fafc-8a43-e235d9f8a922

--- sptest/saml2sp/saml2sp_settings.py
+++ sptest/saml2sp/saml2sp_settings.py
@@ -4,3 +4,11 @@ try:
     IDP_REQUEST_URL = settings.IDP_REQUEST_URL
 except:
     IDP_REQUEST_URL = 'http://127.0.0.1:9000/idp/sso/post/request/'
+
+#XXX: OK, this is an evil hack. But I can't figure out a better way to do this,
+#     since Django requires a local user account. I suppose I could write my
+#     own auth backend, but I don't really want to right now.
+try:
+    SAML_USER_PASSWORD = settings.SAML_USER_PASSWORD
+except:
+    SAML_USER_PASSWORD = settings.SECRET_KEY[::-1]
--- sptest/saml2sp/views.py
+++ sptest/saml2sp/views.py
@@ -1,7 +1,45 @@
+from xml.dom.minidom import parseString
+from django.conf import settings
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
-from saml2sp_settings import IDP_REQUEST_URL
+import saml2sp_settings
+import codex
+
+def _get_email_from_assertion(assertion):
+    """ Returns the email out of the assertion. """
+    #TODO: Wrangle with XML. Ugh.
+    return "dummy@dummy.com"
+
+def _email_to_username(email):
+    """
+    Returns a Django-safe username based on the email address.
+    This is because Django doesn't support email addresses as usernames yet.
+    """
+    return email.replace('@', 'AT')
+
+def _get_user_from_assertion(assertion):
+    """
+    Gets info out of the assertion and locally logs in this user.
+    May create a local user account first.
+    Returns the user object that was created.
+    """
+    email = _get_email_from_assertion(assertion)
+    try:
+        user = User.objects.get(email = email)
+    except:
+        user = User.objects.create_user(
+            _email_to_username(email),
+            email,
+            saml2sp_settings.SAML_USER_PASSWORD
+        )
+    #NOTE: This next line will fail if the user has changed his password
+    #      via the local account. This actually is a good thing, I think.
+    user = authenticate(username=user.username,
+                        password=saml2sp_settings.SAML_USER_PASSWORD)
+    return user
 
 def sso_login(request):
     sso_destination = request.GET.get('next', None)
@@ -9,7 +47,7 @@ def sso_login(request):
     request = 'TODO'
     token = sso_destination
     tv = {
-        'request_url': IDP_REQUEST_URL,
+        'request_url': saml2sp_settings.IDP_REQUEST_URL,
         'request': request,
         'token': token,
         'next': sso_destination,
@@ -21,7 +59,13 @@ def sso_response(request):
     #TODO: Only allow this view to accept POSTs from trusted sites.
     #sso_session = request.session.get('sso_destination', None),
     sso_session = request.POST.get('RelayState', None)
+    data = request.POST.get('SAMLResponse', None)
+    assertion = codex.decode_base64_and_inflate(data)
+    user = _get_user_from_assertion(assertion)
+    login(request, user)
     tv = {
+        'user': user,
+        'assertion': assertion,
         'sso_destination': sso_session,
     }
     return render_to_response('saml2sp/sso_complete.html', tv)
