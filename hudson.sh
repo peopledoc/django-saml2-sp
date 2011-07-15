5a41cf92afbdacef49f4e539124daea747740f37
john@andersoninnovative.com <john@andersoninnovative.com@120867ed-ad96-fafc-8a43-e235d9f8a922>
2011-07-15 15:04:57 +0000

    Only test apps for this project, not Django core or other contrib apps.
    git-svn-id: http://django-saml2-sp.googlecode.com/svn/trunk@14 120867ed-ad96-fafc-8a43-e235d9f8a922

--- hudson.sh
+++ hudson.sh
@@ -2,6 +2,9 @@
 # Hudson CI Test Script - for Linux
 pushd .
 cd sptest
-pwd
-python manage.py test
+echo Testing in directory: `pwd`
+echo `python -V`
+echo Django Version: `python manage.py --version`
+# Only test our apps, not the Django core apps:
+python manage.py test saml2sp
 popd
