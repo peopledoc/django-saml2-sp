6cdc7b4938c1cf623bbd7e4787b508f22de268a6
john@andersoninnovative.com <john@andersoninnovative.com@120867ed-ad96-fafc-8a43-e235d9f8a922>
2011-07-22 17:54:34 +0000

    Changed LOCAL_ROOT to PROJECT_ROOT to avoid confusion; added logout link and template.
    git-svn-id: http://django-saml2-sp.googlecode.com/svn/trunk@21 120867ed-ad96-fafc-8a43-e235d9f8a922

--- sptest/urls.py
+++ sptest/urls.py
@@ -1,19 +1,20 @@
 from django.conf.urls.defaults import *
+from django.contrib.auth.views import logout
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+try:
+    from django.conf import settings
+    logout_url = settings.LOGOUT_URL
+    if logout_url[0] == '/':
+        logout_url = logout_url[1:]
+except:
+    logout_url = 'accounts/logout/'
+
 urlpatterns = patterns('',
-    # Example:
-    # (r'^sptest/', include('sptest.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs'
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
+    url(r'^' + logout_url, logout, name='logout_url'),
     (r'^admin/', include(admin.site.urls)),
-
     (r'^sp/', include('sptest.saml2sp.urls')),
 )
