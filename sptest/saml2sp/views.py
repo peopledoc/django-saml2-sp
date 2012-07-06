79ce4562a7d62c0a3918780e2eeaaf9b44e2d8b2
john@andersoninnovative.com <john@andersoninnovative.com@120867ed-ad96-fafc-8a43-e235d9f8a922>
2012-07-06 18:22:48 +0000

    Merged branch 'attributes' into trunk.
    git-svn-id: http://django-saml2-sp.googlecode.com/svn/trunk@56 120867ed-ad96-fafc-8a43-e235d9f8a922

--- sptest/saml2sp/views.py
+++ sptest/saml2sp/views.py
@@ -11,6 +11,8 @@ from django.core.urlresolvers import reverse
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_view_exempt
+# Other imports
+from BeautifulSoup import BeautifulStoneSoup
 # Local imports
 import base
 import codex
@@ -29,7 +31,7 @@ IDP_CHOICES = (
 )
 class IdpSelectionForm(forms.Form):
     idp = forms.ChoiceField(choices=IDP_CHOICES)
-    
+
 def _get_entity_id(request):
     entity_id = saml2sp_settings.SAML2SP_ENTITY_ID
     if entity_id is None:
@@ -37,16 +39,55 @@ def _get_entity_id(request):
     return entity_id
 
 def _get_email_from_assertion(assertion):
-    """ Returns the email out of the assertion. """
-    #TODO: Wrangle with XML. Ugh.
-    return "dummy@dummy.com"
+    """
+    Returns the email out of the assertion.
+
+    At present, Assertion must pass the email address as the Subject, eg.:
+
+    <saml:Subject>
+            <saml:NameID Format="urn:oasis:names:tc:SAML:2.0:nameid-format:email"
+                         SPNameQualifier=""
+                         >email@example.com</saml:NameID>
+    """
+    soup = BeautifulStoneSoup(assertion)
+    subject = soup.findAll('saml:subject')[0]
+    name_id = subject.findAll('saml:nameid')[0]
+    email = name_id.string
+    return email
 
 def _email_to_username(email):
     """
     Returns a Django-safe username based on the email address.
     This is because Django doesn't support email addresses as usernames yet.
+
     """
     return email.replace('@', 'AT')
+
+def _get_attributes_from_assertion(assertion):
+    """
+    Returns the SAML Attributes (if any) that are present in the assertion.
+
+    NOTE: Technically, attribute values could be any XML structure.
+          But for now, just assume a single string value.
+    """
+    attributes = {}
+    soup = BeautifulStoneSoup(assertion)
+    attrs = soup.findAll('saml:attribute')
+    for attr in attrs:
+        name = attr['name']
+        values = attr.findAll('saml:attributevalue')
+        if len(values) == 0:
+            # Ignore empty-valued attributes. (I think these are not allowed.)
+            continue
+        elif len(values) == 1:
+            #See NOTE:
+            attributes[name] = values[0].string
+        else:
+            # It has multiple values.
+            for value in values:
+                #See NOTE:
+                attributes.setdefault(name, []).append(value.string)
+    return attributes
 
 def _get_user_from_assertion(assertion):
     """
@@ -63,10 +104,13 @@ def _get_user_from_assertion(assertion):
             email,
             saml2sp_settings.SAML2SP_SAML_USER_PASSWORD
         )
-    #NOTE: This next line will fail if the user has changed his password
-    #      via the local account. This actually is a good thing, I think.
+
+    #NOTE: Login will fail if the user has changed his password via the local
+    # account. This actually is a good thing, I think.
     user = authenticate(username=user.username,
                         password=saml2sp_settings.SAML2SP_SAML_USER_PASSWORD)
+    if user is None:
+        raise Exception('Unable to login user "%s" with SAML2SP_SAML_USER_PASSWORD' % email)
     return user
 
 def sso_login(request, selected_idp_url):
@@ -120,12 +164,13 @@ def sso_response(request):
     sso_session = request.POST.get('RelayState', None)
     data = request.POST.get('SAMLResponse', None)
     assertion = base64.b64decode(data)
-    #TODO: Expand this next bit to process/translate attributes, too:
     user = _get_user_from_assertion(assertion)
+    attributes = _get_attributes_from_assertion(assertion)
     login(request, user)
     tv = {
         'user': user,
         'assertion': assertion,
+        'attributes': attributes,
         'sso_destination': sso_session,
     }
     return render_to_response('saml2sp/sso_complete.html', tv)
